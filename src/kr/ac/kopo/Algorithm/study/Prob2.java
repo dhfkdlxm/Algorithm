583cb4032aa289e0ae86e124218d2a11d368c238
YoonJungHwan <dhfkdlxm@naver.com>
2021-04-06 19:01:48 +0900

    "과제 수정"

--- src/kr/ac/kopo/Algorithm/study/Prob2.java
+++ src/kr/ac/kopo/Algorithm/study/Prob2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Prob2 {
 
 	public static void main(String[] args) {
+		//5번 문자열 겹쳐서 출력하기
 		Scanner sc = new Scanner(System.in);
 		System.out.print("문자열을 입력하세요. : ");
 		String str = sc.nextLine();
--- src/kr/ac/kopo/Algorithm/study/SumTest.java
+++ src/kr/ac/kopo/Algorithm/study/SumTest.java
@@ -3,7 +3,7 @@ package kr.ac.kopo.Algorithm.study;
 import java.util.Scanner;
 
 public class SumTest {
-
+	//문제 1번 홀짝 합산 출력하기
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자 입력 : ");

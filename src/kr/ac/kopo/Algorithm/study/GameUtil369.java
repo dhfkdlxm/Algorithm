049eb1e95b5a2f0085659ff5021c02f7b2116147
YoonJungHwan <dhfkdlxm@naver.com>
2021-04-20 18:27:47 +0900

    "COMMIT"

--- src/kr/ac/kopo/Algorithm/study/GameUtil369.java
+++ src/kr/ac/kopo/Algorithm/study/GameUtil369.java
@@ -4,9 +4,26 @@ public class GameUtil369 {
 
 	public static void main(String[] args) {
 		//6번 369게임
+		//369131619
+		String str = "";
 		for (int i = 0; i < 100; i++) {
-			int j = i%10;
-			if(j != 0 && j%3==0) System.out.println(i + "짝");
+			str = str.valueOf(i+1);
+			
+			if (str.contains("3") || str.contains("6") || str.contains("9")) {
+				System.out.print(i);
+			}
+			for (int j = 0; j < str.length(); j++) {
+				if(str.charAt(j) == 3 || str.charAt(j) == 6 || str.charAt(j) == 9) {
+					System.out.print("짝");
+				}
+			}
+			System.out.println();
+			
+			
+			
+			
 		}
+		
+		
 	}
 }
